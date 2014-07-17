e2918258ba00113e53f12a175f30d82e95088aa9
mooreandrew <andrewmoore@Andrews-MacBook-Pro.local>
2014-07-17 11:53:30 +0100

    Added app_host.

--- features/support/config.rb
+++ features/support/config.rb
@@ -2,6 +2,7 @@ Capybara.default_selector = :xpath
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 Capybara.default_wait_time = 10
+Capybara.app_host = 'http://localhost:4567' # change url
 
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, :inspector => true)
