96fcefd56c7f8b1485ce56cc4c92850ede56ebf3
mooreandrew <andrewmoore@Andrews-MacBook-Pro.local>
2014-07-25 15:29:02 +0100

    Testing http auth changes

--- features/support/config.rb
+++ features/support/config.rb
@@ -1,3 +1,9 @@
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
+require 'selenium-webdriver'
+
+include Capybara::DSL
+
 Capybara.default_selector = :xpath
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
@@ -10,3 +16,11 @@ end
 
 $http_auth_name = (ENV['HTTPAUTH_USERNAME'] || '')
 $http_auth_password = (ENV['HTTPAUTH_PASSWORD'] || '')
+
+if page.driver.respond_to?(:basic_auth)
+  page.driver.basic_auth($http_auth_name, $http_auth_password)
+elsif page.driver.respond_to?(:basic_authorize)
+  page.driver.basic_authorize($http_auth_name, $http_auth_password)
+elsif page.driver.respond_to?(:browser) && page.driver.browser.respond_to?(:basic_authorize)
+  page.driver.browser.basic_authorize($http_auth_name, $http_auth_password)
+end
